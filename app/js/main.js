8efb5626d78ca496acf02c87ef8bf498e1b40a3b
Yury <yurykoropenko@gmail.com>
2018-08-16 18:07:31 +0300

    first

---
+++ app/js/main.js
@@ -0,0 +1,15 @@
+/*Текстовый слайдер*/
+	$('.p-lineslider').owlCarousel({
+		loop:true,
+		nav:false,
+		dots: false,
+		animateOut: 'fadeOut',
+		items:1,
+		autoplay:true,
+		autoplayHoverPause:true,
+		autoplayTimeout: 10000,
+
+	});
+$(function() {
+
+});
