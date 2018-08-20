046b9baad5579beed1895778728949dfc9ecc218
Yury <yurykoropenko@gmail.com>
2018-08-20 18:10:06 +0300

    commit

--- app/js/main.js
+++ app/js/main.js
@@ -7,9 +7,15 @@
 		items:1,
 		autoplay:true,
 		autoplayHoverPause:true,
-		autoplayTimeout: 10000,
-
+		autoplayTimeout: 10000
 	});
+	$('.p-scrollslider').owlCarousel({
+		loop:true,
+		nav:false,
+		dots: false,
+		items:5
+	});
+
 $(function() {
 
 });
