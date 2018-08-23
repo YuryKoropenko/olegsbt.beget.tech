7efcdadbde9e1414f1b65f8dfee894740fc86944
Yury <yurykoropenko@gmail.com>
2018-08-23 17:08:26 +0300

    commit

--- app/js/main.js
+++ app/js/main.js
@@ -17,5 +17,74 @@
 	});
 
 $(function() {
+	if( $(window).width() >= 1070 ) {
+		$('.h-nav__item').hover(function() {
+			$('.h-nav__item').removeClass('active');
+			var carrentnav = $(this).children('.h-nav__link').attr('name');
+			if(carrentnav == "togwomen") {
+				$('.h-subnav').hide();
+				$('.h-nav__trang').stop(false, true).animate({
+					left: "30px"
+				}, 300);
+				$(this).addClass('active');
+				$('.h-subnav-1').show();
+			}
+			if(carrentnav == "togmen") {
+				$('.h-subnav').hide();
+				$('.h-nav__trang').stop(false, true).animate({
+					left: "125px"
+				}, 300);
+				$(this).addClass('active');
+				$('.h-subnav-2').show();
+			}
+			if(carrentnav == "togkids") {
+				$('.h-subnav').hide();
+				$('.h-nav__trang').stop(false, true).animate({
+					left: "232px"
+				}, 300);
+				$(this).addClass('active');
+				$('.h-subnav-3').show();
+			}
+		});
 
+		$('.h-subnav__item').hover(function() {
+			$(this).children('.header__menu').stop(false, true).fadeIn(100);
+		}, function() {
+			$(this).children('.header__menu').stop(false, true).hide();
+		});
+	}
+
+	if( $(window).width() <= 1069 ) {
+		$('.h-nav__item').on('click' ,function() {
+			$('.h-nav__item').removeClass('active');
+			var carrentnav = $(this).children('.h-nav__link').attr('name');
+			if(carrentnav == "togwomen") {
+				$('.h-subnav').hide();
+				$('.h-nav__trang').stop(false, true).animate({
+					left: "42px"
+				}, 300);
+				$(this).addClass('active');
+				$('.h-subnav-1').show();
+				return false;
+			}
+			if(carrentnav == "togmen") {
+				$('.h-subnav').hide();
+				$('.h-nav__trang').stop(false, true).animate({
+					left: "135px"
+				}, 300);
+				$(this).addClass('active');
+				$('.h-subnav-2').show();
+				return false;
+			}
+			if(carrentnav == "togkids") {
+				$('.h-subnav').hide();
+				$('.h-nav__trang').stop(false, true).animate({
+					left: "230px"
+				}, 300);
+				$(this).addClass('active');
+				$('.h-subnav-3').show();
+				return false;
+			}
+		});
+	}
 });
