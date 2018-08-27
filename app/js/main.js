a452e0de85927e85406fb17f3b8483bb86db99ea
Yury <yurykoropenko@gmail.com>
2018-08-27 13:53:42 +0300

    commit

--- app/js/main.js
+++ app/js/main.js
@@ -55,6 +55,64 @@ $(function() {
 	}
 
 	if( $(window).width() <= 1069 ) {
+		$('.h-mobnav__link-bars').on('click', function() {
+			if($('.wrapper').hasClass('active') == false) {
+				$('.wrapper').addClass('active');
+				$('.header__mobnav').show();
+				$('.wrapper-bg').show();
+				$('.wrapper-bg').stop(false, true).animate({
+					left: "280px"
+				});
+				$('.wrapper').stop(false, true).animate({
+					left: "280px"
+				});
+				$('.header').stop(false, true).animate({
+					left: "280px"
+				});
+				return false;
+			} else {
+				$('.wrapper').removeClass('active');
+				$('.header__mobnav').hide();
+				$('.wrapper-bg').hide();
+				$('.wrapper-bg').stop(false, true).animate({
+					left: "0"
+				});
+				$('.wrapper').stop(false, true).animate({
+					left: "0"
+				});
+				$('.header').stop(false, true).animate({
+					left: "0"
+				});
+				return false;
+			}
+		});
+		$('.wrapper-bg').on('click', function() {
+			$('.wrapper').removeClass('active');
+			$('.header__mobnav').hide();
+			$('.wrapper-bg').hide();
+			$('.wrapper-bg').stop(false, true).animate({
+				left: "0"
+			});
+			$('.wrapper').stop(false, true).animate({
+				left: "0"
+			});
+			$('.header').stop(false, true).animate({
+				left: "0"
+			});
+			return false;
+		});
+		$('.h-subnav__item-menu .h-subnav__link').on('click', function() {
+			$(this).parent('.h-subnav__item-menu').children('.header__h-menu').animate({
+				left: '0'
+			});
+			return false;
+		});
+		$('.h-menu__link-back').on('click', function() {
+			$('.header__h-menu').animate({
+				left: '280px'
+			});
+			return false;
+		});
 		$('.h-nav__item').on('click' ,function() {
 			$('.h-nav__item').removeClass('active');
 			var carrentnav = $(this).children('.h-nav__link').attr('name');
